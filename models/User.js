e8b23f5e57c83bd2320dc4c3f96cc01b4461bfb5
Alex Fourmy <alexfourmy@gmail.com>
2022-01-11 08:42:24 +0100

    Created a database schema for the User

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const { Schema, model, trusted } = require('mongoose')
+
+const UserSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    timestamp: true
+  }
+)
+
+const User = model('User', UserSchema)
+module.exports = User
