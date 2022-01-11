30700028911c18bbbf128d77fcf7ad75a4781bce
Alex Fourmy <alexfourmy@gmail.com>
2022-01-11 15:14:21 +0100

    Configured cookie-parser

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
+
+const authRoute = require('./routes/auth')
+
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded())
+app.use(cookieParser())
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.send('The app is running!')
 })
 
-app.post('/name', (req, res) => {
-  if (req.body.name) {
-    return res.json({ name: req.body.name })
-  } else {
-    return res.status(400).json({ error: 'No name provided' })
-  }
-})
+app.use('/api/auth', authRoute)
 
 mongoose
   .connect(process.env.MONGO_URI)
