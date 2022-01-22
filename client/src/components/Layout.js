4cc3eb2847edd90dffd54e1778686bcd3736a69f
Alex Fourmy <alexfourmy@gmail.com>
2022-01-22 18:58:28 +0100

    First commit

---
+++ client/src/components/Layout.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Header from './Header'
+import AuthBox from './AuthBox'
+import { useGlobalContext } from '../context/GlobalContext'
+
+const Layout = () => {
+  const { fetchingUser } = useGlobalContext()
+
+  return fetchingUser ? (
+    <div className="loading">
+      <h1>Loading</h1>
+    </div>
+  ) : (
+    <Router>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<AuthBox />} />
+        <Route exact path="/register" element={<AuthBox register />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default Layout
