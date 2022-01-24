499931b826b2a27326f431f2973e33ada23d0d4d
Alex Fourmy <alexfourmy@gmail.com>
2022-01-24 17:07:39 +0100

    Added new actions to the context (markascomplete and markasincomplete)

--- client/src/context/GlobalContext.js
+++ client/src/context/GlobalContext.js
@@ -92,10 +92,52 @@ export const GlobalProvider = props => {
     }
   }
 
+  // action: Add a Todo Item
+  const addToDo = toDo => {
+    dispatch({
+      type: 'SET_INCOMPLETE_TODOS',
+      payload: [toDo, ...state.incompleteToDos]
+    })
+  }
+
+  const toDoComplete = toDo => {
+    dispatch({
+      type: 'SET_INCOMPLETE_TODOS',
+      payload: state.incompleteToDos.filter(
+        incompleteToDo => incompleteToDo._id !== toDo._id
+      )
+    })
+
+    dispatch({
+      type: 'SET_COMPLETE_TODOS',
+      payload: [toDo, ...state.completeToDos]
+    })
+  }
+
+  const toDoIncomplete = toDo => {
+    dispatch({
+      type: 'SET_COMPLETE_TODOS',
+      payload: state.completeToDos.filter(
+        completeToDo => completeToDo._id !== toDo._id
+      )
+    })
+
+    const newIncompleteToDos = [toDo, ...state.incompleteToDos]
+    dispatch({
+      type: 'SET_INCOMPLETE_TODOS',
+      payload: newIncompleteToDos.sort(
+        (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+      )
+    })
+  }
+
   const value = {
     ...state,
     getCurrentUser,
-    logout
+    logout,
+    addToDo,
+    toDoComplete,
+    toDoIncomplete
   }
 
   return (
