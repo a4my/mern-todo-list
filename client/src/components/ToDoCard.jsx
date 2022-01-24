7f0cf6c7d0e0397e3cd936fc43c53e07882dfa88
Alex Fourmy <alexfourmy@gmail.com>
2022-01-24 17:10:02 +0100

    Implemented logic to mark todo items as complete or incomplete

--- client/src/components/ToDoCard.jsx
+++ client/src/components/ToDoCard.jsx
@@ -1,9 +1,12 @@
 import React, { useRef, useState } from 'react'
+import axios from 'axios'
+import { useGlobalContext } from '../context/GlobalContext'
 
 export default function ToDoCard({ toDo }) {
   const [content, setContent] = useState(toDo.content)
   const [editing, setEditing] = useState(false)
   const input = useRef(null)
+  const { toDoComplete, toDoIncomplete } = useGlobalContext()
 
   const onEdit = e => {
     e.preventDefault()
@@ -21,9 +24,29 @@ export default function ToDoCard({ toDo }) {
     setContent(toDo.content)
   }
 
+  const markAsComplete = e => {
+    e.preventDefault()
+
+    axios.put(`/api/todos/${toDo._id}/complete`).then(res => {
+      toDoComplete(res.data)
+    })
+  }
+
+  const markAsIncomplete = e => {
+    e.preventDefault()
+
+    axios.put(`/api/todos/${toDo._id}/incomplete`).then(res => {
+      toDoIncomplete(res.data)
+    })
+  }
+
   return (
     <div className={`todo ${toDo.complete ? 'todo--complete' : ''}`}>
-      <input type="checkbox" checked={toDo.complete} />
+      <input
+        type="checkbox"
+        checked={toDo.complete}
+        onChange={!toDo.complete ? markAsComplete : markAsIncomplete}
+      />
 
       <input
         type="text"
